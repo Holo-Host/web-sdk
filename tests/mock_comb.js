9a66d3a95b2caf8013f2ec39ce0aa33ab1fe77e5
Robbie Carlton <robbie.carlton@gmail.com>
2021-01-28 10:25:59 -0700

    new formatting

--- tests/mock_comb.js
+++ tests/mock_comb.js
@@ -2,23 +2,23 @@
 let next_response;
 
 global.COMB = {
-    async connect () {
-	return Promise.resolve({
-	    call() {
-		if ( next_response === undefined )
-		    return `{"Err":"Next response is undefined"}`
-		else {
-		    const value			= next_response;
-		    next_response		= undefined;
-		    return value;
-		}
-	    }
-	});
-    }
+  async connect() {
+    return Promise.resolve({
+      call() {
+        if (next_response === undefined)
+          return `{"Err":"Next response is undefined"}`
+        else {
+          const value = next_response;
+          next_response = undefined;
+          return value;
+        }
+      }
+    });
+  }
 }
 
 module.exports = {
-    nextResponse ( value ) {
-	     next_response				= value;
-    }
+  nextResponse(value) {
+    next_response = value;
+  }
 }
--- tests/unit/test_api.js
+++ tests/unit/test_api.js
@@ -1,92 +1,92 @@
-const path				= require('path');
-const log				= require('@whi/stdlog')(path.basename( __filename ), {
-    level: process.env.LOG_LEVEL || 'fatal',
+const path = require('path');
+const log = require('@whi/stdlog')(path.basename(__filename), {
+  level: process.env.LOG_LEVEL || 'fatal',
 });
 
-const expect				= require('chai').expect;
+const expect = require('chai').expect;
 
 require("../mock_browser.js");
-const mock_comb				= require("../mock_comb.js");
-const { Connection }			= require("../../src/index.js");
+const mock_comb = require("../mock_comb.js");
+const { Connection } = require("../../src/index.js");
 
 
 describe("Javascript API", () => {
 
-    it("should call zome function", async () => {
-	const envoy			= new Connection();
-	await envoy.ready();
+  it("should call zome function", async () => {
+    const envoy = new Connection();
+    await envoy.ready();
 
-	mock_comb.nextResponse({
-	"Ok":{
-		"balance":		"0",
-		"credit":		"0",
-		"payable":		"0",
-		"receivable":	"0",
-		"fees":		"0",
-		"available":	"0",
-	}
-	});
-
-	const response		= await envoy.zomeCall(
-	"holofuel", "transactions", "ledger_state"
-	);
-
-	log.debug("Response: %s", response );
-	const state			= response.Ok;
-
-	expect( state		).to.be.an("object");
-	expect( Object.keys(state)	).to.have.members([
-	"balance", "credit", "payable", "receivable", "fees", "available"
-	]);
+    mock_comb.nextResponse({
+      "Ok": {
+        "balance": "0",
+        "credit": "0",
+        "payable": "0",
+        "receivable": "0",
+        "fees": "0",
+        "available": "0",
+      }
     });
 
-    it("should call app_info", async () => {
-	const installed_app_id = 'app_id'
-	const envoy			= new Connection();
-	await envoy.ready();
+    const response = await envoy.zomeCall(
+      "holofuel", "transactions", "ledger_state"
+    );
 
-	const expectedResponse 	= {
-	installed_app_id,
-	cell_data: [[['hash', 'pubkey'], 'dna_alias']]
-	};
+    log.debug("Response: %s", response);
+    const state = response.Ok;
 
-	mock_comb.nextResponse(expectedResponse);
+    expect(state).to.be.an("object");
+    expect(Object.keys(state)).to.have.members([
+      "balance", "credit", "payable", "receivable", "fees", "available"
+    ]);
+  });
 
-	const response		= await envoy.appInfo(installed_app_id);
+  it("should call app_info", async () => {
+    const installed_app_id = 'app_id'
+    const envoy = new Connection();
+    await envoy.ready();
 
-	log.debug("Response: %s", response );
+    const expectedResponse = {
+      installed_app_id,
+      cell_data: [[['hash', 'pubkey'], 'dna_alias']]
+    };
 
-	expect( response		).to.be.an("object");
-	expect( response 		).to.equal(expectedResponse)
-    });
+    mock_comb.nextResponse(expectedResponse);
 
-    describe("ready", () => {
-	let globalComb;
-	const expectedError = 'timeout error';
+    const response = await envoy.appInfo(installed_app_id);
 
-	before(() => {
-	    globalComb = global.COMB;
-	    global.COMB = {
-		connect () {
-		    throw new Error(expectedError);
-		}
-	    }
-	})
+    log.debug("Response: %s", response);
 
-	after(() => {
-	    global.COMB = globalComb;
-	})
-	
-	it("throws an error from COMB", async () => {
-	    let thrownMessage;
-   	    const envoy			= new Connection();
-	    try {
-		await envoy.ready();
-	    } catch (e)	{
-		thrownMessage = e.message;
-	    }
-	    expect( thrownMessage 	).to.equal(expectedError);
-	})
-    });
+    expect(response).to.be.an("object");
+    expect(response).to.equal(expectedResponse)
+  });
+
+  describe("ready", () => {
+    let globalComb;
+    const expectedError = 'timeout error';
+
+    before(() => {
+      globalComb = global.COMB;
+      global.COMB = {
+        connect() {
+          throw new Error(expectedError);
+        }
+      }
+    })
+
+    after(() => {
+      global.COMB = globalComb;
+    })
+
+    it("throws an error from COMB", async () => {
+      let thrownMessage;
+      const envoy = new Connection();
+      try {
+        await envoy.ready();
+      } catch (e) {
+        thrownMessage = e.message;
+      }
+      expect(thrownMessage).to.equal(expectedError);
+    })
+  });
 
 });
