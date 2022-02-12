e71dc0d2fc8b28461bb5dd0db51bd4dfdeb10013
jetttech <ldjetton21@gmail.com>
2022-02-11 19:29:44 -0600

    update nix env

---
+++ config.nix
@@ -0,0 +1,6 @@
+{
+  # git ls-remote https://github.com/holochain/holonix staging
+  holonixRevision = "61955a8fa318cab42cbf6013e52b266f363aaf8b";
+
+  holochainVersionId = "v0_0_116";
+}
--- default.nix
+++ default.nix
@@ -1,46 +1,14 @@
-{ pkgs ? import ./pkgs.nix {} }:
-
-with pkgs;
-
-{
-  web-sdk = stdenv.mkDerivation rec {
-    name = "web-sdk";
-    src = gitignoreSource ./.;
-
-    buildInputs = [
-      holochain-rust
-      python
-
-      dnaPackages.holofuel
-    ];
-
-    nativeBuildInputs = [
-      nodejs
-    ];
-
-    preConfigure = ''
-      cp -r ${npmToNix { inherit src; }} node_modules
-      chmod -R +w node_modules
-      patchShebangs node_modules
-    '';
-
-    buildPhase = ''
-      npm run build
-    '';
-
-    installPhase = ''
-      mkdir $out
-      mv dist build node_modules $out
-    '';
-
-    fixupPhase = ''
-      patchShebangs $out
-    '';
-
-    checkPhase = ''
-      make test
-    '';
-
-    doCheck = true;
+let
+  config = import ./config.nix;
+  holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/${config.holonixRevision}.tar.gz";
+  holonix = import (holonixPath) {
+    inherit (config) holochainVersionId;
   };
+  nixpkgs = holonix.pkgs;
+in nixpkgs.mkShell {
+  inputsFrom = [ holonix.main ];
+  packages = with nixpkgs; [
+    binaryen
+    nodejs-16_x
+  ];
 }
